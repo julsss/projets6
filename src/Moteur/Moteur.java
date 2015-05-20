2ff76f2917229bc41d07e91a1cb4744d042cac3e
whitefecaf <mickaeldufrenoy@gmail.com>
2015-05-20 17:18:49 +0200

    mise àjoueur
    a faire modifier joueur, humain et ordi facile

--- src/Moteur/Moteur.java
+++ src/Moteur/Moteur.java
@@ -1,6 +1,9 @@
 package Moteur;
 
 import Modele.Historique;
+import Modele.Humain;
+import Modele.Joueur;
+import Modele.OrdiFacile;
 import Modele.Plateau;
 
 //import package ihm
@@ -9,7 +12,7 @@ public class Moteur{
   
 	private Historique historique;
 	private Plateau plateau;
-	
+	private Joueur joueur1, joueur2, joueur3, joueur4;
 	public static final int PIONNOIR = 0;
 	public static final int PIONBLANC = 1;
 	public static final int PIONBLEU = 2;
@@ -27,22 +30,35 @@ public class Moteur{
 	 historique = new Historique();
 	 if(nbJoueur == 2)
 	 {
-		creationJoueur(j1); 
+		joueur1 = creationJoueur(j1); 
+		joueur2 = creationJoueur(j2);
 	 }
  }
   
  public Moteur(int nbJoueur, int j1, int j2, int j3, int j4){
 	 plateau = new Plateau(5, 5, nbJoueur);
 	 historique = new Historique();
-	 if(nbJoueur == 2)
+	 if(nbJoueur == 4)
 	 {
-		creationJoueur(j1); 
+		joueur1 = creationJoueur(j1); 
+		joueur2 = creationJoueur(j2);
+		joueur3 = creationJoueur(j3);
+		joueur4 = creationJoueur(j4);
 	 }
  }
   
   
-private void creationJoueur(int j1) {
-	// TODO Auto-generated method stub
+private Joueur creationJoueur(int j) {
+	if(j == HUMANPLAYER)
+	{
+		return new Humain(false, true);
+	}
+	else if(j == IAEasy)
+	{
+		return new OrdiFacile();
+	}
+	System.out.println("erreur type de joueur inconnu");
+	return null;
 	
 }
 
@@ -60,5 +76,37 @@ public Plateau getPlateau() {
 }
 public void setPlateau(Plateau plateau) {
 	this.plateau = plateau;
+}
+
+public Joueur getJoueur1() {
+	return joueur1;
+}
+
+public void setJoueur1(Joueur joueur1) {
+	this.joueur1 = joueur1;
+}
+
+public Joueur getJoueur2() {
+	return joueur2;
+}
+
+public void setJoueur2(Joueur joueur2) {
+	this.joueur2 = joueur2;
+}
+
+public Joueur getJoueur3() {
+	return joueur3;
+}
+
+public void setJoueur3(Joueur joueur3) {
+	this.joueur3 = joueur3;
+}
+
+public Joueur getJoueur4() {
+	return joueur4;
+}
+
+public void setJoueur4(Joueur joueur4) {
+	this.joueur4 = joueur4;
 }
 }
