bb3b85a75e13764582f49bd0efe9f0d2ae55c359
loucasse93 <loucasse93@hotmail.fr>
2015-05-22 10:55:01 +0200

    MAJ fonction aléatoire de OrdiFacile

--- src/Modele/OrdiFacile.java
+++ src/Modele/OrdiFacile.java
@@ -1,9 +1,8 @@
 package Modele;
 import java.awt.*;
 import java.util.*;
-//import Moteur.Coup;
-import Moteur.Moteur;
 
+import Moteur.*;
 
 public class OrdiFacile extends Joueur {
 
@@ -17,50 +16,12 @@ public class OrdiFacile extends Joueur {
 
 	private static Random rand = new Random();
 
-	public static Point facile(int [][] gaufre, Moteur m){
-		Point coup = new Point();
-		boolean valide = false;
-		//boolean perdant= false;
-		while(!valide){
-			coup.x= rand.nextInt(gaufre.length);
-			coup.y = rand.nextInt(gaufre[0].length);
-
-			//valide = m.coup_valide(gaufre, coup.x, coup.y);
-
-			//perdant = m.coup_perdant(gaufre, coup.x, coup.y);
-		}
-		return coup;
+	public Coup facile(Moteur m){	
+		ArrayList<Coup> liste = m.listeCoupPossible(this);
+		
+		int taille = liste.size();
+		int val = rand.nextInt(taille);
+		
+		return liste.get(val);
 	}
-
-	/*public int alphabeta(int p, int alpha, int beta, Moteur m){
-		alpha = Integer.MIN_VALUE;
-		beta = Integer.MAX_VALUE;
-		if(plateau.getNbPionNoirWin() == 3 || plateau.getNbPionBlancWin() == 3 || p == 0){
-			return eval();
-		}
-		Coup meilleur_coup;
-		Coup m;
-		//for(){
-			plateau.joue_coup(m);
-			int score = - alphabeta(p-1,-beta,-alpha,plateau);
-			plateau.annuler_coup(m);
-			if(score >= alpha){
-				alpha = score;
-				meilleur_coup = m;
-				if(alpha >= beta){
-					break;
-				}
-			}
-		//}
-			
-		return alpha;
-	}
-*/
-	
-
-	private int eval() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
 }
