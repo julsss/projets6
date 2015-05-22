7b956279d404e6a12f39eb948a192454cbaa73e6
loucasse93 <loucasse93@hotmail.fr>
2015-05-22 09:13:03 +0200

    Ajout getter/setter J1 et J2 dans Moteur Ebauche de la fonction eval dans l'IA

--- src/Modele/OrdiMoyen.java
+++ src/Modele/OrdiMoyen.java
@@ -41,8 +41,45 @@ public class OrdiMoyen extends Joueur {
 */
 	
 
-	private int eval() {
-		// TODO Auto-generated method stub
+	double [][] evalPlacementJ1 = {
+			{4,   3,   2,   1,   0},
+			{4.5, 3.5, 2.5, 1.5, 1},
+			{5,   4,   3,   2.5, 2},
+			{5.5, 4.5, 4,   3.5, 3},
+			{6,   5.5, 5,   4.5, 4}
+		};
+
+	double [][] evalPlacementJ2 = {
+			{4,4,4,4,4},
+			{3,3,3,3,4},
+			{2,2,2,3,4},
+			{1,1,2,3,4},
+			{0,1,2,3,4}
+		};
+		
+	//Passage d'un moteur en parametre : j1 -> m.j1, etc...
+	private int eval(Moteur m) {
+	
+		int score = 0;
+		
+		//Nombre de billes perso
+		score += 5 - m.j1.getNbBilles();
+		
+		//Nombre de billes adverses		
+		score -= 5 - j2.getNbBilles();
+		
+		
+		//Distance euclidienne des billes Joueur 1 et Joueur 2
+		for(int i=0;i<p.getNbCol();i++){
+			for(int j=0;i<p.getNbLigne();j++){
+				if(p.get(c.i).get(c.j) == Case.PJ1 && j1 == this.j1)
+					score += evalPlacementJ1[i][j];
+				else if(p.get(c.i).get(c.j) == Case.PJ2 && j1 == this.j2)
+					score -= evalPlacementJ2[i][j];
+			}
+		}
+		
+		
 		return 0;
 	}
 
