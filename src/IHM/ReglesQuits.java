a35aeae8ce0142d5f3e72631098519de59824829
judith <>
2015-06-02 11:28:08 +0200

    modif interface

--- src/IHM/ReglesQuits.java
+++ src/IHM/ReglesQuits.java
@@ -1,9 +1,7 @@
 package IHM;
-import javax.swing.BoxLayout;
+import javax.swing.SpringLayout;
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.FontMetrics;
-import java.awt.GradientPaint;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.*;
@@ -21,17 +19,12 @@ public class ReglesQuits implements Runnable,ActionListener{
     
     public void run(){
         
-        frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-        frame.setSize(350,700);
-        frame.setVisible(true);
-        frame.setLocationRelativeTo(null);
-        
-        JPanel panel = new JPanel();
-        panel.setBackground(Color.BLACK);
-        GridLayout gl = new GridLayout(3,1);
-        panel.setLayout(gl);
-        
+        frame.setBackground(Color.BLACK);
+        SpringLayout sl = new SpringLayout();
+        //panel.setLayout(gl);
+        frame.setLayout(sl);
         ImageIcon image = new ImageIcon("res/Quits.png");
+        JLabel test = new JLabel(image);
         
         JButton menu = new JButton("Menu");
         //JLabel label = new JLabel("");
@@ -40,20 +33,30 @@ public class ReglesQuits implements Runnable,ActionListener{
         //label.setFont(font);
         //label.setOpaque(true);
         //label.setBackground(Color.GREEN);
-        panel.add(new JLabel(image));
-        JTextArea textArea = new JTextArea(30, 20);
+        //panel.add(new JLabel(image));
+        JTextArea textArea = new JTextArea(12, 22);
+        textArea.setEditable(false);
         textArea.setText("-> Le premier joueur qui parvient à faire sortir 3 de ses billes de la case opposée à son camp de départ remporte la partie. \n -> Déplacer une de ses billes d'une case, à gauche, à droite ou en diagonale vers l'avant. Il est donc interdit de reculer.Ou faire glisser une rangée ou une colonne en prenant une case à une extrémité, puis en l'insérant de l'autre côté. Cela a pour effet de décaler les cases (donc les billes qui s'y trouvent). C'est de cette manière que l'on peut éjecter une bille du plateau.");
         textArea.setFont(font);
         textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
         
         menu.addActionListener(this);
-        //panel.add(label);
-        panel.add(textArea);
-        panel.add(menu);
+        frame.add(test);
+        frame.add(textArea);
+        frame.add(menu);
         
-        frame.add(panel);
+        //positionnement des composants
+        sl.putConstraint(SpringLayout.WEST, test, 80, SpringLayout.HORIZONTAL_CENTER, frame);
+        sl.putConstraint(SpringLayout.WEST, textArea, 8, SpringLayout.WEST, frame);
+        sl.putConstraint(SpringLayout.NORTH, textArea, 125, SpringLayout.NORTH, frame);
+        sl.putConstraint(SpringLayout.WEST, menu, 130, SpringLayout.HORIZONTAL_CENTER, frame);
+        sl.putConstraint(SpringLayout.NORTH, menu, 350, SpringLayout.SOUTH, frame);
         
-        
+        frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        frame.setSize(350,700);
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
     }
     
     public void actionPerformed(ActionEvent e){
